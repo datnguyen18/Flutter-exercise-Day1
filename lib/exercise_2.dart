8520cf30c17cc5ffc1d7d4a1ced71a6af7ebcbc3
Dat Nguyen <dnt@elca.vn>
2021-01-25 15:01:36 +0700

    delete unused package and comments

--- lib/exercise_2.dart
+++ lib/exercise_2.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-// Uncomment lines 7 and 10 to view the visual layout at runtime.
-// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  // debugPaintSizeEnabled = true;
   runApp(MyApp());
 }
 
