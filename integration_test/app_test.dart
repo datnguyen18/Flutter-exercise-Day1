bf38c81009570834a3437488df9b25a11b580b57
Dat Nguyen <dnt@elca.vn>
2021-01-25 14:28:28 +0700

    add exercise 2

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:flutter_app/main.dart' as app;
+import 'package:flutter_app/exercise_1.dart' as app;
 
 void main() => run(_testMain);
 
---
+++ lib/exercise_2.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ConvertTool();
+  }
+}
+
+class ConvertTool extends StatefulWidget {
+  @override
+  _ConvertToolState createState() => _ConvertToolState();
+}
+
+class _ConvertToolState extends State<ConvertTool> {
+  final celsiusController = TextEditingController();
+  final fahrenheitController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    celsiusController.dispose();
+    fahrenheitController.dispose();
+    super.dispose();
+  }
+
+  void _handleChangeCToF(String text) {
+      String fahrenheit = '';
+      if(celsiusController.text.isNotEmpty) {
+        fahrenheit = '${double.parse(celsiusController.text)* 1.8 +32}';
+      }
+      fahrenheitController.text = fahrenheit;
+  }
+
+  void _handleChangeFToC(String text) {
+      String celsius = '';
+      if(fahrenheitController.text.isNotEmpty) {
+        celsius = '${(double.parse(fahrenheitController.text)-32)/1.8}';
+      }
+      celsiusController.text = celsius;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Convert tool'),
+        ),
+        body:Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Celsius °C'
+              ),
+              controller: celsiusController,
+              onChanged: _handleChangeCToF,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: 'Fahrenheit °F'
+              ),
+              controller: fahrenheitController,
+              onChanged: _handleChangeFToC,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
