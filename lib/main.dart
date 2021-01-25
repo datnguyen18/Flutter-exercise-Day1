a2cf7cdb4fe4f9f8b5d2dfa3923815cb535c553e
Dat Nguyen <dnt@elca.vn>
2021-01-25 11:15:40 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    child: Column(
+                                      children: [
+                                        Expanded(
+                                          child: Square(Colors.brown),
+                                        ),
+                                        Expanded(
+                                          child: Square(Colors.purple),
+                                        ),
+                                        Expanded(
+                                          child: Square(Colors.teal),
+                                        ),
+                                        Expanded(
+                                          child: Square(Colors.green),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    child: Column(
+                                      children: [
+                                        Expanded(
+                                          flex: 3,
+                                          child: Container(
+                                            color: Colors.orangeAccent,
+                                          ),
+                                        ),
+                                        Expanded(
+                                          flex: 1,
+                                          child: Container(
+                                            child: Row(
+                                              children: [
+                                                Expanded(
+                                                  flex: 1,
+                                                  child: Container(
+                                                    color: Colors.limeAccent,
+                                                  ),
+                                                ),
+                                                Expanded(
+                                                  flex: 1,
+                                                  child: Container(
+                                                    color: Colors.cyan,
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.brown,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.pink,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.red,
+                    margin: EdgeInsets.only(right: 10),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              child: Container(
+                width: 150,
+                height: 150,
+                color: Colors.black12,
+              ),
+              top: 350,
+              left: 30,
+            ),
+            Positioned(
+              child: FloatingActionButton(
+                child: Icon(Icons.play_arrow),
+              ),
+              bottom: 20,
+              right: 20,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Square extends StatelessWidget {
+  Color _color;
+  Square(this._color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: this._color,
+    );
+  }
+}
